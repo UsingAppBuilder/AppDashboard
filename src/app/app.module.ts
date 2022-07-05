2dfc2195aad0599d1ca2ef9d5a57db917cbd941c
UsingAppBuilder <96213792+UsingAppBuilder@users.noreply.github.com>
2022-07-05 17:03:55 -0300

    Dashboard App updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,14 @@ import { BrowserModule, HammerModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MasterViewComponent } from './master-view/master-view.component';
-import { IgxCardModule, IgxGridModule, IgxButtonModule, IgxRippleModule, IgxIconModule, IgxAvatarModule, IgxListModule } from 'igniteui-angular';
+import { MasterView1Component } from './master-view1/master-view1.component';
+import { IgxButtonModule, IgxRippleModule, IgxIconModule, IgxAvatarModule, IgxListModule, IgxCardModule, IgxGridModule } from 'igniteui-angular';
 import { IgxCategoryChartModule } from 'igniteui-angular-charts';
 import { FormsModule } from '@angular/forms';
-import { MasterView1Component } from './master-view1/master-view1.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MasterViewComponent,
     MasterView1Component
   ],
   imports: [
@@ -20,15 +18,15 @@ import { MasterView1Component } from './master-view1/master-view1.component';
     HammerModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    IgxCardModule,
-    IgxGridModule,
-    IgxCategoryChartModule,
-    FormsModule,
     IgxButtonModule,
     IgxRippleModule,
     IgxIconModule,
     IgxAvatarModule,
-    IgxListModule
+    IgxListModule,
+    IgxCardModule,
+    IgxGridModule,
+    IgxCategoryChartModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
